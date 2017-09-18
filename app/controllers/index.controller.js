327610f114c982ad03d03437e70bfa49ed59ce47
Panchorn Lertvipada <panchorn.ler@ascendcorp.com>
2017-09-19 02:11:59 +0700

    Connect to mongodb, and create local login logic

--- app/controllers/index.controller.js
+++ app/controllers/index.controller.js
@@ -1,7 +1,20 @@
-exports.index = (req, res) => {
-	res.send('Hello World');
-}
+var path = require("path");
 
 exports.help = (req, res) => {
 	res.send('Help me');
+}
+
+exports.index = (req, res) => {
+	res.sendFile((path.join(__dirname + '/../views/index.html')));
+}
+
+exports.home = (req, res) => {
+	if (req.user) {
+		res.sendFile((path.join(__dirname + '/../views/home.html')));
+	}
+	else {
+		res.status(400).send({
+			message: 'User is not signed in'
+		});
+	}
 }
